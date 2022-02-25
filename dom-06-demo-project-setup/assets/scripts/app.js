6b5fec434a26cc6cc88de3cac297243a9e2898c6
Monica Delgado <mnca.delgado@gmail.com>
2022-02-25 10:41:25 -1100

    worked with selecting buttons, and storing input info

---
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -0,0 +1,112 @@
+const addMovieModal = document.getElementById('add-modal');
+/*Other ways to selec the button 
+const addMovieModel = document.querySelector('#add-modal');
+const addMovieModel = document.body.children[1];
+*/
+const startAddMovieButton = document.querySelector('header button')
+/*Other ways to select the button
+const startAddMovieButton = document.querySelector('header').lastElementChild;
+*/
+const backdrop = document.getElementById('backdrop');
+/*other way to select
+const backdrop = document.body.firstElementChild;
+*/
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+
+//practicing list selection
+const userInputs = addMovieModal.querySelectorAll('input');
+/*other way to select
+const userInput = addMovieModal.getElementsByTagName('input')
+*/
+
+const entryTextSection = document.getElementById('entry-text');
+
+const movies = [];
+
+
+
+const updateUI = () => {
+    if (movies.length === 0) {
+        entryTextSection.style.display = 'block';
+    } else {
+        entryTextSection.style.display = 'none';
+    }
+};
+
+const renderNewMovieElement = (tittle, imageUrl, rating) => {
+    const newMovieElement = document.createElement('li');
+    newMovieElement.className = 'movie-element';
+    newMovieElement.innerHTML = `
+      <div class="movie-element__image">
+         <img src="${imageUrl}" alt="${tittle}">
+      </div>
+      <div class"movie-element__info">
+         <h2>${tittle}</h2>
+         <p>${rating}/5 stars</p>
+      </div>
+    `;
+    const listRoot = document.getElementById('movie-list');
+    listRoot.append(newMovieElement);
+};
+
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+}
+
+const toggleMovieModal = () => {
+    addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
+};
+
+const clearMovieInput = () => {
+    for (const userInput of userInputs) {
+        userInput.value = '';
+    }
+};
+
+const cancelAddMovieHandler = () => {
+    toggleMovieModal();
+    clearMovieInput();
+
+};
+
+const addMovieHandler = () => {
+    const tittleValue = userInputs[0].value;
+    const imageUlrValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value
+
+    if (
+        tittleValue.trim() === '' ||
+        imageUlrValue.trim() === '' ||
+        ratingValue.trim() === '' ||
+        +ratingValue < 1 ||
+        +ratingValue > 5
+        ) {
+            alert('Please enter valid values (rating between 1 and 5).');
+            return;
+        }
+    
+        const newMovie = {
+            tittle: tittleValue,
+            image: imageUlrValue,
+            rating: ratingValue
+        };
+
+    movies.push(newMovie);
+    console.log(movies);
+    toggleMovieModal();
+    clearMovieInput();
+    renderNewMovieElement(newMovie.tittle, newMovie.image, newMovie.rating);
+    updateUI();
+};
+
+const backdropClickHandler = () => {
+toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler)
